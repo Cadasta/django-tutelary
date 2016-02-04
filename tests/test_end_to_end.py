32d58cf250208aaabdd68437e4d5b071a62dca2a
Ian Ross <iross@cadasta.org>
2016-02-04 16:14:35 +0100

    Add failing end-to-end test for collective actions

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -29,6 +29,11 @@ def setup(datadir, db):
         body=datadir.join('project-policy.json').read()
     )
     prj_pol.save()
+    prj_deny_pol = Policy.objects.create(
+        name='prj',
+        body=datadir.join('parcel-list-deny-policy.json').read()
+    )
+    prj_deny_pol.save()
 
     user1.assign_policies(def_pol)
     user2.assign_policies(def_pol,
@@ -36,13 +41,15 @@ def setup(datadir, db):
     user3.assign_policies(def_pol,
                           (org_pol, {'organisation': 'Cadasta'}),
                           (prj_pol, {'organisation': 'Cadasta',
-                                     'project': 'TestProj'}))
+                                     'project': 'TestProj'}),
+                          (prj_deny_pol, {'organisation': 'Cadasta',
+                                          'project': 'Proj2'}))
 
-    return (user1, user2, user3, def_pol, org_pol, prj_pol)
+    return (user1, user2, user3, def_pol, org_pol, prj_pol, prj_deny_pol)
 
 
 def test_basic(datadir, setup):  # noqa
-    user1, user2, user3, def_pol, org_pol, prj_pol = setup
+    user1, user2, user3, def_pol, org_pol, prj_pol, prj_deny_pol = setup
 
     obj1 = Object('parcel/Cadasta/TestProj/123')
     assert not user1.has_perm('parcel.edit', obj1)
@@ -58,3 +65,34 @@ def test_basic(datadir, setup):  # noqa
     assert not user1.has_perm('parcel.view', obj3)
     assert not user2.has_perm('parcel.view', obj3)
     assert not user3.has_perm('parcel.view', obj3)
+
+
+def test_collective(datadir, setup):  # noqa
+    user1, user2, user3, def_pol, org_pol, prj_pol, prj_deny_pol = setup
+
+    parcel1 = Object('parcel/Cadasta/TestProj/123')
+    proj1 = Object('project/Cadasta/TestProj')
+    assert not user1.has_perm('parcel.list', parcel1)
+    assert not user1.has_perm('parcel.list', proj1)
+    assert user2.has_perm('parcel.list', parcel1)
+    assert user2.has_perm('parcel.list', proj1)
+    assert not user3.has_perm('parcel.list', parcel1)
+    assert not user3.has_perm('parcel.list', proj1)
+
+    parcel2 = Object('parcel/Cadasta/Proj2/114')
+    proj2 = Object('project/Cadasta/Proj2')
+    assert not user1.has_perm('parcel.list', parcel2)
+    assert not user1.has_perm('parcel.list', proj2)
+    assert user2.has_perm('parcel.list', parcel2)
+    assert user2.has_perm('parcel.list', proj2)
+    assert not user3.has_perm('parcel.list', parcel2)
+    assert not user3.has_perm('parcel.list', proj2)
+
+    parcel3 = Object('parcel/SkunkWorks/SR-71/Area51')
+    proj3 = Object('project/SkunkWorks/SR-71')
+    assert not user1.has_perm('parcel.list', parcel3)
+    assert not user1.has_perm('parcel.list', proj3)
+    assert not user2.has_perm('parcel.list', parcel3)
+    assert not user2.has_perm('parcel.list', proj3)
+    assert not user3.has_perm('parcel.list', parcel3)
+    assert not user3.has_perm('parcel.list', proj3)
