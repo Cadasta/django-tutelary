67dd8f53b613f83f3ed618295aecc874b0f02dbd
Ian Ross <iross@cadasta.org>
2016-04-21 22:41:35 +0200

    Tidying

--- setup.py
+++ setup.py
@@ -58,9 +58,6 @@ version = get_version(package)
 
 
 if sys.argv[-1] == 'publish':
-    if os.system("pip freeze | grep wheel"):
-        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
-        sys.exit()
     if os.system("pip freeze | grep twine"):
         print("twine not installed.\nUse `pip install twine`.\nExiting.")
         sys.exit()
